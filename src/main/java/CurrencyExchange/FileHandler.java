d8df0e4fe194ea9dd417b0063b6ea8c587e7c6cd
Ivan <ilee3379@uni.sydney.edu.au>
2020-10-05 22:44:52 +1100

    refactored add() method to prevent invalid values from being added to currencies.txt, added more tests for the add() and find() methods, as well as implemented a private helper method in the FileHandlerTest class

--- src/main/java/CurrencyExchange/FileHandler.java
+++ src/main/java/CurrencyExchange/FileHandler.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class FileHandler {
     /*
-    If a currency is already in the file currencies.txt, it will return its value. Otherwise it will return null.
+    If a currency is already in the file currencies.txt, it will return its name and value as a csv. Otherwise it will return null.
      */
     public static String find(String currency) {
         String result = null;
         if(currency != null && !currency.isEmpty()) {
-            String line = null;
+            String line;
             boolean found = false;
             Scanner currencies = null;
 
@@ -28,23 +28,43 @@ public class FileHandler {
                     result = line;
                 }
             }
+            currencies.close();
         }
 
         return result;
     }
 
     public static void add(String currency, float value) {
-        PrintWriter writer = null;
-        try {
-            writer = new PrintWriter(new FileOutputStream("currencies.txt", true));
-        }
-        catch(FileNotFoundException e) {
-            System.out.println("error opening currencies.txt");
-            System.exit(1);
+        boolean valid = true;
+
+        if(value <= 0) {
+            System.out.println("value must be greater than 0");
+            valid = false;
         }
 
-        writer.println(String.join(",", currency, Float.toString(value)));
-        writer.close();
+        if(currency == null || currency.isEmpty()) {
+            System.out.println("Enter valid values for currency");
+            valid = false;
+        }
+
+        if(FileHandler.find(currency) != null) {
+            System.out.println("currency already loaded in currencies.txt");
+            valid = false;
+        }
+
+        if(valid) {
+            PrintWriter writer = null;
+            try {
+                writer = new PrintWriter(new FileOutputStream("currencies.txt", true));
+            }
+            catch(FileNotFoundException e) {
+                System.out.println("error opening currencies.txt");
+                System.exit(1);
+            }
+
+            writer.print(System.lineSeparator() + String.join(",", currency, Float.toString(value)));
+            writer.close();
+        }
     }
 
     public static void remove(String currency) {
--- src/test/java/CurrencyExchange/FileHandlerTest.java
+++ src/test/java/CurrencyExchange/FileHandlerTest.java
@@ -1,35 +1,117 @@
 package CurrencyExchange;
 
+import org.junit.After;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.*;
+import java.util.Scanner;
+
 import CurrencyExchange.FileHandler;
 
 public class FileHandlerTest {
+    /*
+    Removes the last line from currencies.txt. This is to restore the file currencies.txt to its previous state when
+    tests modify its contents, e.g. when successfully invoking the add function.
+     */
+    private void removeLastLine() {
+        Scanner currencies = null;
+        PrintWriter tempFile = null;
+        File temp = new File("tempFile");
+        File input = new File("currencies.txt");
+
+        try{
+            tempFile = new PrintWriter(temp);
+            currencies = new Scanner(input);
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Can't open files");
+            System.exit(0);
+        }
+
+        String line;
+        boolean secondLastLine = false;
+        tempFile.print(currencies.nextLine());
+        while(currencies.hasNextLine() && !secondLastLine) {
+            line = currencies.nextLine();
+            if(!currencies.hasNextLine()) {
+                // We've reached the second last line
+                secondLastLine = true;
+            }
+            else {
+                tempFile.print(System.lineSeparator() + line);
+            }
+        }
+        currencies.close();
+        tempFile.close();
+
+        boolean rename = temp.renameTo(input);
+    }
+
+
 
     @Test
-    void find() {
+    void findSuccess() {
         String line = null;
         line = FileHandler.find("USD");
         assertEquals("USD,1", line, "USD found in Currencies.txt");
     }
 
     @Test
-    void findNull() {
+    void findWithEmptyInput() {
         String line = null;
         line = FileHandler.find("");
         assertNull(line, "null input, null output");
     }
 
     @Test
-    void findDoesntExist() {
+    void findWithNullInput(){
+        String empty = null;
+        assertNull(FileHandler.find(empty));
+    }
+
+    @Test
+    void findNonExistingEntry() {
         String line = null;
         line = FileHandler.find("NotFound");
         assertNull(line, "String \'NotFound\' not in currency.txt");
     }
 
     @Test
-    void add() {
+    void addSuccessfully() {
+        String result = null;
+        if(FileHandler.find("doesntExist") == null) {
+            FileHandler.add("doesntExist", 1);
+            result = FileHandler.find("doesntExist");
+        }
+        assertNotNull(result);
+        removeLastLine();
+    }
 
+
+    @Test
+    void addNegativeValue() {
+        if(FileHandler.find("negativeCurrency") == null) {
+            FileHandler.add("negativeCurrency", -1);
+        }
+        assertNull(FileHandler.find("negativeCurrency"));
+    }
+
+    @Test
+    void addExistingCurrency() {
+        FileHandler.add("USD", 2);
+        // method add() should prevent USD,2 from being added to currencies.txt
+        assertNotEquals("USD,2", FileHandler.find("USD"));
+    }
+
+    @Test
+    void addNoCurrency() {
+        String currency = null;
+        FileHandler.add(currency, 1);
+        assertNull(FileHandler.find(currency)); // currency should not have been added to file
+        currency = "";
+        FileHandler.add(currency,1);
+        assertNull(FileHandler.find(currency));
     }
 
     @Test
