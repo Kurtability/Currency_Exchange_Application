b668b5e904a6f353270f0b9dda928164ce9143b8
Ashton Whitting <ashtonwhitting132@gmail.com>
2020-10-07 02:32:50 +1100

    added remove function to fileHandler

--- src/main/java/CurrencyExchange/FileHandler.java
+++ src/main/java/CurrencyExchange/FileHandler.java
@@ -1,12 +1,14 @@
 package CurrencyExchange;
+import java.awt.List;
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class FileHandler {
     /*
     If a currency is already in the file currencies.txt, it will return its name and value as a csv. Otherwise it will return null.
      */
-    public static String find(String currency) {
+     public static String find(String currency) {
         String result = null;
         if(currency != null && !currency.isEmpty()) {
             String line;
@@ -67,10 +69,43 @@ public class FileHandler {
         }
     }
     
+    // update a currencies price that is currently stored
+    public static void update(String currency) {
+
+    }
+    
     // remove a currency from the database
     public static void remove(String currency) {
+        try {
+            ArrayList<String> database = new ArrayList<String>();
+            Scanner reader = new Scanner(new File("currencies.txt"));
+            boolean found = false;
 
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                if (!line.contains(currency)) {
+                    database.add(line);
+                } else {
+                    found = true;
+                }
+            }
+            reader.close();
+            if (found == false) {
+                System.out.println("Currency could not be removed as it does not currently exist in the databse");
+            } else {
+                PrintWriter writer = new PrintWriter(new File("currencies.txt"));
+                int count = 0;
+                while (count < database.size()) {
+                    writer.println(database.get(count));
+                    count++;
+                }
+                writer.close();
+            }
 
+        } catch(FileNotFoundException e) {
+            System.out.println("error opening currencies.txt");
+            System.exit(1);
+        }
     }
 }
 
