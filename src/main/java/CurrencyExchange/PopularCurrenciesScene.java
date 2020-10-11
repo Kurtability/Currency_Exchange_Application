2db580963def7d2ab598007e6845b06d2e17a1fb
Pratham Purohit <prathamjp02@gmail.com>
2020-10-11 16:40:50 +1100

    Added Arrows to popular currencies

--- src/main/java/CurrencyExchange/PopularCurrenciesScene.java
+++ src/main/java/CurrencyExchange/PopularCurrenciesScene.java
@@ -168,9 +168,9 @@ public class PopularCurrenciesScene {
         String return_string = ((String) Double.toString(now));
         return_string = String.format("%.2f",now);
         if(pre >now){
-            return_string =  return_string + "↓";
+            return_string =  return_string + "\u2193";
         }else if(pre<now){
-            return_string =  return_string + "↑";
+            return_string =  return_string + "\u2191";
         }else{
             return_string =  return_string+ "-";
         }
