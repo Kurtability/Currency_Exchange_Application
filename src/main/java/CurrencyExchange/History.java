b8abdbc0ed4e7ccf34d73dc6e7486a63dfc8b4f2
Ashton Whitting <ashtonwhitting132@gmail.com>
2020-10-11 21:43:59 +1100

    updating Summary scene

--- src/main/java/CurrencyExchange/History.java
+++ src/main/java/CurrencyExchange/History.java
@@ -57,14 +57,13 @@ public class History {
         //double min_currency2=min(values_of_Currency2);
         double ssd_currency1_2=standarddeviation(curr1_to_curr2);
         //double ssd_currency2=standarddeviation(values_of_Currency2);
-        String summary1= (currency1 + "average:"+ average_currency1+ " , median:"+ median_currency1+ " , maximum:"+max_currency1+ ", minimum:"+min_currency1+ ", standard deviation:"+ ssd_currency1);
-        String summary2=(currency2 + "average:"+ average_currency2+" , median:"+ median_currency2+ ", maximum:"+max_currency2+", minimum:"+min_currency2+ ", standard deviation:"+ssd_currency2);
+        String summary1= (currency1 + "average:"+ average_currency1_currency2+ " , median:"+ median_currency1_2+ " , maximum:"+max_currency1_2+ ", minimum:"+min_currency1_2+ ", standard deviation:"+ ssd_currency1_2);
         //String summary=summary1+ summary2;
         //new String summary= String();
 
         //return (currency1 + "average:"+ average_currency1+ " , median:"+ median_currency1+ " , maximum:"+max_currency1+ ", minimum:"+min_currency1+ ", standard deviation:"+ ssd_currency1);
         if(isValid){
-            return summary1+ summary2;
+            return summary1;
         }
         else if(currency1_exists==false || currency2_exists==false){
             return ("Currency doesnt exist");
@@ -72,9 +71,6 @@ public class History {
         else{
             return ("Proper Date not entered");
         }
-
-
-
     }
 
 
